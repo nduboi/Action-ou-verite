36c0962a0e1f9f7dffa6c1749a0e8fe16d20f686
Nduboi <63715705+nduboi@users.noreply.github.com>
2025-08-07 00:48:29 +0200

    fix: :bug: Fix error with wrong environment variable

--- api_server/src/config/db.js
+++ api_server/src/config/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_ROOT_PASSWORD,
+    password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
     waitForConnections: true,
     connectionLimit: 10,
